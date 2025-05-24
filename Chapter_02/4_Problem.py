25feb8615dc98c899ede65afac968a45c8eeb12b
sunny kumar <147946977+sunnykumar77399@users.noreply.github.com>
2025-05-24 12:48:21 +0530

    Add files via upload

---
+++ Chapter_02/4_Problem.py
@@ -0,0 +1,32 @@
+
+# printing reminder
+"""
+x =48
+b=7
+print("Reminder is :",x%b)
+"""
+
+# check variable type using iput variable
+"""
+a= input("Enter somthing :")
+t=type(a)
+print(t)
+
+"""
+# check a is greater then b or not
+"""
+a=34
+b=80
+result=a>b 
+print(result) 
+"""
+# finding the avg of two number
+"""
+v= int(input("Enter 1st number :"))
+u= int(input("Enter 2nd number :"))
+print("the avg of two number is :",(v+u)/2)
+
+"""
+# finding square of a number
+x= int(input("Enter 1st number :"))
+print("square of number is :",x**2)
