0ec6c3c678d5cfe4e9d8c879a31357b0760993c9
sunny kumar <147946977+sunnykumar77399@users.noreply.github.com>
2025-05-24 12:48:39 +0530

    Add files via upload

---
+++ Chapter_02/variable_1.py
@@ -0,0 +1,12 @@
+
+a=1 # a is an integer
+
+b=2.5 # b is a floating point number
+
+_c="sunny" # c is string
+
+d= False # d is boolean variable
+
+e=None # e is a none type variable 
+
+print("output is :",a+b)
