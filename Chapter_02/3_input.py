c8a125426c77183c7a99db1a0a309f271ffa558c
sunny kumar <147946977+sunnykumar77399@users.noreply.github.com>
2025-05-24 12:47:56 +0530

    Add files via upload

---
+++ Chapter_02/3_input.py
@@ -0,0 +1,15 @@
+"""
+a= input("Enter a number :")
+b= input("enter a number :")
+
+print("the sum of a nd b is :",a+b)
+# there is output will be str concatinate
+# if a = 2 and b= 3 then sum will be 23
+
+"""
+
+a= int(input("Enter a number :"))
+b= int(input("Enter a number :"))
+# b= input("enter a number :")
+
+print("the sum of a nd b is :",a+b)
