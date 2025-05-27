a4d79e579cdbe505f200e09011e10c120a92ce5c
sunny kumar <147946977+sunnykumar77399@users.noreply.github.com>
2025-05-27 09:59:02 +0530

    Add files via upload

---
+++ Chapter_04/2_list_method.py
@@ -0,0 +1,24 @@
+friends=["apple","orange",2.65362,6, False ,"sunny"]
+# print(friends)
+print(friends[0])
+
+# unlike string list are mutable 
+friends.append("kumar")
+print(friends)
+
+# list sorting
+number=[1,4,67,7,7,7,7,4,43,4,4,5]
+number.sort()
+print(number)
+
+# REverse 
+number.reverse()
+print(number)
+
+# #insert
+number.insert(2,45)
+print(number)
+
+# delete 
+# number.pop(5)
+print(number.pop(6))
