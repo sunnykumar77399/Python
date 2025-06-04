9fd056a7006cf5547575876b748d731db5da0d24
sunny kumar <147946977+sunnykumar77399@users.noreply.github.com>
2025-06-04 19:25:28 +0530

    Create 1_dictionary.py

---
+++ Chapter_05/1_dictionary.py
@@ -0,0 +1,16 @@
+mark={
+    "sunny" : 80,
+    "ankit" : 90,
+    "gaurav" : 100
+}
+
+print(mark,type(mark)) 
+# print(mark[0])  wrong method
+print(mark["sunny"])
+"""
+1. It is unordered
+2. It is mutable
+3. It is indexed
+4. Cannot contain duplicate keys
+
+"""
