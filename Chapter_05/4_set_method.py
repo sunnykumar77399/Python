e0e0aea2ea39464a90fbcf80f4b100a659959f51
sunny kumar <147946977+sunnykumar77399@users.noreply.github.com>
2025-06-04 19:26:38 +0530

    Add files via upload

---
+++ Chapter_05/4_set_method.py
@@ -0,0 +1,16 @@
+"""
+s={1,2,4,6,"sunny",3}
+print(type(s))
+
+s.add(45)
+print(s)
+s.remove(4)
+print(s)
+
+"""
+
+s1={1,3,5,6,2}
+s2={1,4,5,8,2}
+ 
+print(s1.union(s2))
+print(s1.intersection(s2))
