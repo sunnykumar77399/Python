cc21e7232271567dbb7a7d8fc54c37b8a493ffaf
sunny kumar <147946977+sunnykumar77399@users.noreply.github.com>
2025-06-23 16:44:11 +0530

    Create 1_conditionals.py

---
+++ Chapter_06/1_conditionals.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number : "))
+
+if(a>18):
+    print("you are a adult ")
+    print("if condition executed")
+else: 
+    print("you are not adult\nelse executed")
+print("program end")
